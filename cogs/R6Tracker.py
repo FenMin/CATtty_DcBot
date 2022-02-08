7ab0b90314903991765a69561673aacdb909f83b
fenmin <fenming891564z@yahoo.com>
2022-02-08 23:35:52 +0800

    trash code

--- cogs/R6Tracker.py
+++ cogs/R6Tracker.py
@@ -1,31 +1,18 @@
 import discord
 from discord.ext import commands
 
+
+
 class R6Tracker(commands.Cog):
     def __init__(self , bot):
         self.bot = bot
     
-    async def r6(self,ctx):
-        await ctx.send("未完工啦")
+
+
+    
     
     
         
-        
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 def setup(bot):
--- cogs/command.py
+++ cogs/command.py
@@ -23,23 +23,37 @@ class command(commands.Cog):
         await ctx.channel.purge(limit=1)
    
     @commands.command(brief="請輸入創頻數量")
-    async def voice_create(self, ctx, num):         
+    async def create_voice(self, ctx, num):         
         if num.isdigit():
             await ctx.send(f'創建 ***{num}*** 個語音頻道') 
             for i in range(int(num)):
-                await ctx.guild.create_voice_channel(f"第{i+1}個")
+                await ctx.guild.create_voice_channel(f"第{i+1}個語音頻道")
         else:
             await ctx.send("請輸入數字")
        
-    @commands.command(brief="刪除開頭為 \"第\" 的頻道")
+    @commands.command(brief="刪除開頭為 \"第\" 結尾為 \"個\" 的頻道")
     async def delete_channel(self, ctx):
-        channel = ctx.guild.voice_channels
+        channel = ctx.guild.channels
         for i in channel:  
-            if i.name[0] == "第" and i.name[-1] == "個":  
+            if i.name[0] == "第" and (i.name[-4:] == "語音頻道" or i.name[-4:] == "文字頻道"):  
                 await i.delete()
-                await ctx.send(f'以刪除 ***{i.name}*** 頻道')
+                await ctx.send(f'以刪除 ***{i.name}*** 之 ***{i.type}*** 頻道')
 
-        
+    @commands.command(brief="請輸入創頻數量")
+    async def create_text(self, ctx, num):         
+        if num.isdigit():
+            await ctx.send(f'創建 ***{num}*** 個文字頻道') 
+            for i in range(int(num)):
+                await ctx.guild.create_text_channel(f"第{i+1}個文字頻道")
+        else:
+            await ctx.send("請輸入數字")
+
+    @commands.command()
+    async def test(self , ctx):
+        #guild = self.bot.guilds
+        #for i in guild:
+        #    await ctx.send(f'{i.owner} , {type(i.owner)}')
+        await ctx.send(ctx.author.voice)
 
 def setup(bot):
     bot.add_cog(command(bot))
